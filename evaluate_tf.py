a8214030e0c97fd8e3a7f2a6525f8863481c028a
Nguyen Hai Duong <haiduong667799@gmail.com>
2021-06-17 22:52:05 -0900

    test with photo cp

--- evaluate_tf.py
+++ evaluate_tf.py
@@ -173,17 +173,17 @@ def eval_tf(all_checkpoint_path, index_cp, result_folder):
 if __name__ == '__main__':
 
     # change model name
-    model_name = 'b0_ver_1'
-    result_folder = result_all_model + '/photo_cp_' + model_name 
-    all_checkpoint_path = '/home/duongnh/project_f19/photo_attack/result/'+ model_name + '/train/checkpoint_' + model_name 
-    # change list of cp will be evaluated
-    index_cp_list = [ 'cp_12.h5' ]
-    for index_cp in index_cp_list:
-        eval_tf(all_checkpoint_path, index_cp, result_folder) 
+    # model_name = 'b0_ver_1'
+    # result_folder = result_all_model + '/photo_cp_' + model_name 
+    # all_checkpoint_path = '/home/duongnh/project_f19/photo_attack/result/'+ model_name + '/train/checkpoint_' + model_name 
+    # # change list of cp will be evaluated
+    # index_cp_list = [ 'cp_12.h5' ]
+    # for index_cp in index_cp_list:
+    #     eval_tf(all_checkpoint_path, index_cp, result_folder) 
 
     model_name = 'b0_ver_2'
     result_folder = result_all_model + '/photo_cp_' + model_name 
-    all_checkpoint_path = '/home/duongnh/project_f19/photo_attack/result' + '/train/checkpoint_' + model_name 
+    all_checkpoint_path = '/home/duongnh/project_f19/photo_attack/result/'+ model_name + '/train/checkpoint_' + model_name 
     # change list of cp will be evaluated
     index_cp_list = [ 'cp_16.h5' ]
     for index_cp in index_cp_list:
@@ -191,7 +191,7 @@ if __name__ == '__main__':
     
     model_name = 'b1_ver_1'
     result_folder = result_all_model + '/photo_cp_' + model_name 
-    all_checkpoint_path = '/home/duongnh/project_f19/photo_attack/result' + '/train/checkpoint_' + model_name 
+    all_checkpoint_path = '/home/duongnh/project_f19/photo_attack/result/'+ model_name + '/train/checkpoint_' + model_name 
     # change list of cp will be evaluated
     index_cp_list = [ 'cp_04.h5' ]
     for index_cp in index_cp_list:
@@ -199,7 +199,7 @@ if __name__ == '__main__':
 
     model_name = 'b4_ver_1'
     result_folder = result_all_model + '/photo_cp_' + model_name 
-    all_checkpoint_path = '/home/duongnh/project_f19/photo_attack/result' + '/train/checkpoint_' + model_name 
+    all_checkpoint_path = '/home/duongnh/project_f19/photo_attack/result/'+ model_name + '/train/checkpoint_' + model_name 
     # change list of cp will be evaluated
     index_cp_list = ['cp_02.h5', 'cp_04.h5' ]
     for index_cp in index_cp_list:
